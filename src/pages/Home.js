947f1add9ab63be8286e479b11e912d07fc7ebd3
Mohd Aman Azam <amancode006@gmail.com>
2022-11-13 19:46:04 +0530

    push4

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,21 +4,24 @@ import { Button, Form, Input, Layout, Select, InputNumber } from "antd";
 import { Country, State, City } from "country-state-city";
 import "antd/dist/antd.css";
 import { useNavigate } from "react-router-dom";
+import background from "../images/img4.jpg";
 
 const { Option } = Select;
 const { Content } = Layout;
 const Home = () => {
   //   console.log(Country.getAllCountries());
-  console.log(State.getStatesOfCountry("IN"));
+  // console.log(State.getStatesOfCountry("IN"));
   const navigate = useNavigate();
   return (
     <div>
-      <NavBar />
       <Content
         style={{
-          padding: "40px 80px",
-          textAlign: "center",
-          justifyContent: "center",
+          backgroundImage: `url(${background})`,
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center",
+          height: "100vh",
+          width: "100vw",
         }}
       >
         <div>
